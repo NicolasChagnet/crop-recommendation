b41f270aa6d9b34b52ce3b783e01169395dbf6dd
Nicolas Chagnet <nchagnet@protonmail.com>
2024-05-24 21:50:31 +0200

    fixed EDA report

--- src/visualization/visualization_eda.py
+++ src/visualization/visualization_eda.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-from src.config import eda_profile_raw
-from src.data.load_data import check_existing_data, load_dataset_raw
+from src import config
 
 
 def make_report_eda_raw():
     """Generates a complete EDA report of the raw dataset"""
-    check_existing_data()
-    data = load_dataset_raw()
-    ProfileReport(df=data, type_schema={"Crop": "categorical"}).to_file(eda_profile_raw)
+    data = pd.read_csv(config.data_path_raw)
+    ProfileReport(df=data, type_schema={"Crop": "categorical"}).to_file(
+        config.eda_profile_raw
+    )
